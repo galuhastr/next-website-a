0983f6204c94fc096c0624ebe3d8bcf5c5d909b6
GaluhAS <galuhabdstr@gmail.com>
2023-12-14 20:30:13 +0700

    update major

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import Image from "next/image"
+import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="border-2 border-red-500 flexbetween max-container
+    padding-container relative z-30 py-5">
+    <Link href="/">
+        <Image src="/trip-logo.svg" alt="logo" width={74} height={24}/>
+    </Link>
+    </nav>
   )
 }
 
