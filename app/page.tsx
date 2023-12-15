897c5c9b996c15ac9017201d9e7a56cb18a356d5
GaluhAS <galuhabdstr@gmail.com>
2023-12-15 17:06:13 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+import Features from '@/components/Features'
+import GetApp from '@/components/GetApp'
+import Guide from '@/components/Guide'
+import Hero from '@/components/Hero'
+import Tour from '@/components/Tour'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <>
-    
+      <Hero />
+      <Tour />
+      <Guide />
+      <Features />
+      <GetApp />
     </>
   )
 }
