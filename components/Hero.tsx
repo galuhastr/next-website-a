951aba5ef7d8ef87a0952109db498b035c74f134
GaluhAS <galuhabdstr@gmail.com>
2023-12-16 16:02:57 +0700

    update minor

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import React from 'react'
+import Button from './Button'
 
 const Hero = () => {
   return (
     <section className='max-container padding-container flex flex-col
-    gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row border-2 border-red-500'>
+    gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row'>
       <div className='hero-map'/>
 
       {/* LEFT */}
@@ -17,7 +18,7 @@ const Hero = () => {
           width={50}
           height={50}
         />
-        <h1 className='bold-52 lg:bold-88 mt-5 text-purple-50'>Putuk Truno Camp Area</h1>
+        <h1 className='bold-52 lg:bold-88 mt-5 text-purple-50'>Moyo Island</h1>
         <p className='regular-16 mt-6 text-gray-30 xl:max-w-[520px]'>
           We want to be on each of your journeys seeking the
           satisfaction of seeing the incorruptible beauty of nature.
@@ -36,6 +37,58 @@ const Hero = () => {
               />
             ))}
           </div>
+
+          <p className='bold-16 lg:bold-20 text-purple-50'>
+            110k
+            <span className='regular-16 lg:regular-20 ml-1'>
+              Excellent Reviews
+            </span>
+          </p>
+        </div>
+
+        <div className='flex flex-col w-full gap-3 sm:flex-row'>
+          <Button 
+            type='button' 
+            title='Download App' 
+            variant='btn_green'
+            />
+          <Button 
+            type='button' 
+            title='How We Work?'
+            icon='/play.svg'
+            variant='btn_white_text'
+            />
+        </div>
+      </div>
+
+      <div className='relative flex flex-1 items-start'>
+        <div className='relative z-20 flex w-[268px] flex-col gap-8
+        rounded-3xl bg-green-90 px-7 py-8'>
+
+            <div className='flex flex-col'>
+              <div className='flexBetween'>
+                <p className='regular-16 text-gray-20'>Location</p>
+                <Image 
+                  src='/close.svg'
+                  alt='close'
+                  width={24}
+                  height={24}
+                  />
+              </div>
+              <p className='bold-20 text-white'>Nusa Tenggara Barat</p>
+            </div>
+
+            <div className='flexBetween'>
+              <div className='flex flex-col'>
+                <p className='regular-16 block text-gray-20'>Distance</p>
+                <p className='bold-20 text-white'>1,195 km</p> 
+              </div>
+              <div className='flex flex-col'>
+                <p className='regular-16 block text-gray-20'>Area</p>
+                <p className='bold-20 text-white'>349 km2</p> 
+              </div>
+            </div>
+
         </div>
       </div>
     </section>
