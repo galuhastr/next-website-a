1cda987b02d3b78f38341b917b351a5afdc58dd4
GaluhAS <galuhabdstr@gmail.com>
2023-12-17 20:28:13 +0700

    update

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,8 +1,16 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Guide = () => {
   return (
-    <div>Guide</div>
+    <section className='flexCenter flex-col'>
+      <div className='border-2 border-red-500 padding-container
+      max-container w-full pb-24'>
+      <Image
+        src='/camp.svg' alt='map' width={50} height={50}
+      />
+      </div>
+    </section>
   )
 }
 
