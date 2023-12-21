a783ea00d1483849154145a8cedc77721d365c75
GaluhAS <galuhabdstr@gmail.com>
2023-12-21 20:19:45 +0700

    update

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,11 +1,26 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Features = () => {
   return (
     <section className='border border-red-500 flex-col flexCenter
     overflow-hidden bg-feature-bg bg-center no-repeat py-24'>
+      <div className='max-container padding-container relative w-full
+      justify-end'>
+        <div className='flex flex-1 lg:min-h-[900px]'>
+          <Image
+            src='/phone.png'
+            alt='phone'
+            width={440}
+            height={1000}
+            className='feature-phone'
+            />
+        </div>
 
+        <div className='z-20 flex flex-col w-full lg:w-[60%]'>
 
+        </div>
+      </div>
     </section>
   )
 }
